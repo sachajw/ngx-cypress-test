372e6451e56a1bb5b3c47219ebea1502c91b0735
Sacha Wharton <sacha@gmail.com>
2020-07-24 16:30:31 +0200

    web tables

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -251,11 +251,12 @@ describe('Our first suite', () => {
       cy.contains('Tables & Data').click()
       cy.contains('Smart Table').click()
 
+      //1
       cy.get('tbody').contains('tr','Larry').then(tableRow => {
         cy.wrap(tableRow).find('.nb-edit').click()
         cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
-        cy.wrap(tableRow).find('.nb-checkmark')
-        cy.wrap(tableRow).find('td').eq(6).should('contain','25')
+        cy.wrap(tableRow).find('.nb-checkmark').click()
+        cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
       })
 
       //2
@@ -263,7 +264,7 @@ describe('Our first suite', () => {
       cy.get('thead').find('tr').eq(2).then(tableRow => {
         cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem')
         cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar')
-        cy.wrap(tableRow).find('nb-checkmark').click()
+        cy.wrap(tableRow).find('.nb-checkmark').click()
       })
       cy.get('tbody tr').first().find('td').then(tableColumns => {
         cy.wrap(tableColumns).eq(2).should('contain','Artem')
@@ -271,9 +272,18 @@ describe('Our first suite', () => {
       })
 
       //3
-      cy.get('thead [placeholder="Age"]').type('20')
-      cy.get('tabe tr').each(tableRow => {
-        cy.wrap(tableRow).first
+      //array with test data
+      const age = [20, 30, 40]
+
+      cy.wrap(age).each(age => {
+        cy.get('thead [placeholder="Age"]').clear().type(age)
+        cy.wait(500)
+        cy.get('tbody tr').each(tableRow => {
+          if(age == 200){
+            cy.wrap(tableRow).should('contain', 'No data found')
+          } else {
+            cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+          }
 
       })
 
